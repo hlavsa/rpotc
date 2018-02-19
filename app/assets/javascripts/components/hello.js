4f4fa435f3aad7813cd8767d476d05dd53d1bba6
hlavsa <hlavsa.jara@seznam.cz>
2018-02-19 16:03:28 +0100

    Initial commit

---
+++ app/assets/javascripts/components/hello.js
@@ -0,0 +1,5 @@
+window.Hello = createReactClass({
+  render: function() {
+    return <h1>{this.props.title}</h1>
+  }
+})
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'pages/home'
+  get 'pages/detail'
+  get 'pages/entity'
+
+  get 'pages/advanced_search'
+
+  root to: 'pages#home'
+end
