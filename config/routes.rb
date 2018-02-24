7545a3e8a4501981c2cea34f4cd7c560113b6d6f
hlavsa <hlavsa.jara@seznam.cz>
2018-02-24 15:10:19 +0100

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get 'pages/home'
-  get 'pages/detail'
-  get 'pages/entity'
-  get 'pages/advanced_search'
-  get 'pages/accreditation_form'
-
   root to: 'pages#home'
+
+  get 'home' => 'pages#home', as: :home
+  get 'detail' => 'pages#detail', as: :detail
+  get 'entity' => 'pages#entity', as: :entity
+  get 'advanced_search' => 'pages#advanced_search', as: :advanced_search
+  get 'accreditation_form' => 'pages#accreditation_form', as: :accreditation_form
+  
 end
